656ac8d6e77f9c7b1091b98aeb92e99c0bc2cb73
thanishar25am-png <thanisha.r_25am@kgkite.ac.in>
2026-01-28 08:40:59 +0530

    problem_052 solved

--- problems/medium/problem_052.py
+++ problems/medium/problem_052.py
@@ -8,8 +8,6 @@ def convert_budget(value):
     
     factor = 0.5
     
-    return value // factor 
-     
-    return value + factor
+    return value // factor, value + factor
 
 print(convert_budget(100))
