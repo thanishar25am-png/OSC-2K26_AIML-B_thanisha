4260cbd20986a063055ec49818ca95a6774bd78c
thanishar25am-png <thanisha.r_25am@kgkite.ac.in>
2026-01-29 14:04:04 +0530

    problem_087 solved

--- problems/medium/problem_087.py
+++ problems/medium/problem_087.py
@@ -6,7 +6,6 @@ Difficulty: Medium
 
 def convert_weight(value):
     factor = 0.5
-    return value // factor
-    return value + factor
+    return value // factor, value + factor
 
 print(convert_weight(100))
