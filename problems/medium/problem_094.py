c03efa8332e87439b5ce18e41178913c2ceac706
thanishar25am-png <thanisha.r_25am@kgkite.ac.in>
2026-01-29 14:12:07 +0530

    problem_094 solved

--- problems/medium/problem_094.py
+++ problems/medium/problem_094.py
@@ -6,7 +6,6 @@ Difficulty: Medium
 
 def convert_time(value):
     factor = 0.5
-    return value // factor
-    return value + factor
+    return value // factor , value + factor
 
 print(convert_time(100))
