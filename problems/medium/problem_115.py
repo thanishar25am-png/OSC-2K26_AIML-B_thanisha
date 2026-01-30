24fc00ca7434874d815da1b3f3c35e8bd6ed0ce4
thanishar25am-png <thanisha.r_25am@kgkite.ac.in>
2026-01-30 08:34:19 +0530

    problem_115 solved

--- problems/medium/problem_115.py
+++ problems/medium/problem_115.py
@@ -6,7 +6,6 @@ Difficulty: Medium
 
 def convert_speed(value):
     factor = 0.5
-    return value // factor
-    return value + factor
+    return value // factor, value + factor
 
 print(convert_speed(100))
